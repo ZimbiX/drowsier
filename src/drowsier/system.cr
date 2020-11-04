b8bad9293a9812d66503d87184afed3d8d2b02bf
Brendan Weibrecht <brendan@weibrecht.net.au>
2020-11-04 23:42:19 +1100

    Run all system commands in a shell for simplicity, and output the command before running for debugging

--- src/drowsier/system.cr
+++ src/drowsier/system.cr
@@ -3,37 +3,42 @@ module Drowsier
     def initialize(@config : Config)
     end
 
-    def lock_screen!
+    def lock_screen! : Void
       puts "Locking screen"
-      `#{config.lock_screen_command}`
+      run(config.lock_screen_command).wait
     end
 
-    def turn_off_screen!
+    def turn_off_screen! : Void
       puts "Turning off screen"
-      `#{config.turn_off_screen_command}`
+      run(config.turn_off_screen_command).wait
     end
 
-    def turn_on_screen!
+    def turn_on_screen! : Void
       puts "Turning off screen"
-      `#{config.turn_on_screen_command}`
+      run(config.turn_on_screen_command).wait
     end
 
-    def screen_off?
-      `#{config.check_if_screen_is_off_command}`
-      $?.success?.tap { |off| puts(off ? "Screen is off" : "Screen is on") }
+    def screen_off? : Bool
+      run(config.check_if_screen_is_off_command).wait
+        .success?.tap { |off| puts(off ? "Screen is off" : "Screen is on") }
     end
 
-    def pause_media!
-      `#{config.pause_media_command}`
+    def pause_media! : Void
+      run(config.pause_media_command).wait
     end
 
-    def play_audio_notification!
+    def play_audio_notification! : Void
       stop_existing_audio_notification!
-      @audio_notification_process = Process.new(config.play_audio_notification_command, shell: true)
+      @audio_notification_process = run(config.play_audio_notification_command)
     end
 
-    private def stop_existing_audio_notification!
+    private def stop_existing_audio_notification! : Void
       audio_notification_process.try &.terminate
+    end
+
+    private def run(command)
+      puts command
+      Process.new(command, shell: true)
     end
 
     private getter config
