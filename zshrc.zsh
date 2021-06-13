b005f28de0277bdc04c5d4cca13ff57997e65cca
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-06-13 21:05:57 +1000

    Use full path to drowsier in zshrc to permit sourcing it first thing

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,9 +1,9 @@
 # Intercepts Ctrl+C, Ctrl+\, Ctrl+Z
-if command -v drowsier &>/dev/null; then
+if [[ -f ~/.local/bin/drowsier ]]; then
   setopt PROMPT_SUBST 2>/dev/null
   PS1_ORIG=$PS1
   export PS1=$'$(kill -HUP $$)'
-  drowsier --tty --silent || kill -HUP $$
+  ~/.local/bin/drowsier --tty --silent || kill -HUP $$
   PS1=$PS1_ORIG
   unset PS1_ORIG
 fi
